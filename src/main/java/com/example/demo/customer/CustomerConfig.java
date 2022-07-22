18d7ba5b6565b38eb405acc1d590798e81bc511a
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2022-07-22 17:23:16 -0400

    Application Properties and Profiles

--- src/main/java/com/example/demo/customer/CustomerConfig.java
+++ src/main/java/com/example/demo/customer/CustomerConfig.java
@@ -1,9 +1,12 @@
 package com.example.demo.customer;
 
+import com.example.demo.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfig {
@@ -11,9 +14,24 @@ public class CustomerConfig {
     @Value("${app.useFakeCustomerRepo:false}")
     private boolean useFakeCustomerRepo;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    private final Environment environment;
+
+    @Autowired
+    public CustomerConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
-    CommandLineRunner commandLineRunner(){
-        return args -> System.out.println("Command line runner hooray");
+    CommandLineRunner commandLineRunner(InfoApp infoApp){
+        return args -> {
+            System.out.println("Command line runner hooray");
+            System.out.println("Company Name: " + companyName);
+            System.out.println("Version: " + environment.getProperty("info.app.version"));
+            System.out.println(infoApp);
+        };
     }
 
     @Bean
